231858c39326b07cb62d65d7239ec70aaea23fcf
JamesManningR <56916766+JamesManningR@users.noreply.github.com>
2021-05-13 23:30:51 +0100

    Added pre-requisites to store for theming

---
+++ components/accessibility/AccessibilityMenu.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="accessibility-menu">
+    <label>
+      Colourblind Theme
+      <!-- <input type="checkbox" @change="setColorBlind($event)" /> -->
+    </label>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class AccessibilityMenu extends Vue {
+  // setColorBlind(evt): void {
+  //   this.$state.dispatch('setColorBlind', evt.target.value)
+  // }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="app">
+  <div class="app theme--light">
     <app-header />
     <Nuxt />
   </div>
